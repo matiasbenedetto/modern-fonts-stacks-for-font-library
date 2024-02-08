e8f65a47653eb7737ec53f42ce586a1971e913cd
Matias Benedetto <matias.benedetto@gmail.com>
2024-02-08 13:03:57 -0300

    implements changes added for gutenberg 17.7  compatibility

--- index.php
+++ index.php
@@ -3,20 +3,19 @@
 Plugin Name: Modern Fonts Stacks
 Plugin URI: https://github.com/matiasbenedetto/modern-fonts-stacks-for-wp-font-library/
 Description: Add a font collection to your WordPress Font Library.
-Version: 0.0.3
-Author: Various Authors
+Version: 0.0.4
+Author: Matias Benedetto
 Author URI: https://wordpress.org/
 License: GPLv2 or later
-Text Domain: modern-fonts-stacks
+Text Domain: modern-fonts-stacks-for-wp-font-library
 */
 
-$modern_fonts_config = array (
-    'slug'          => 'modern-fonts-stacks',
-    'name'        => 'Modern Fonts Stacks',
-    'description' => 'Stacks of modern systems fonts, not font face assets needed. The look will vary on each system.',
-    'src'         => path_join( __DIR__, 'collection.json' ),
-);
-
 if ( function_exists( 'wp_register_font_collection' ) ) {
-    wp_register_font_collection ( $modern_fonts_config );
+
+    function register_modern_fonts_stacks() {
+        wp_register_font_collection ( 'modern-fonts-stacks', path_join( __DIR__, 'collection.json' ) );
+    }
+
+    add_action( 'init', 'register_modern_fonts_stacks' );
+    
 }
